1ad863f127abb5f3506801a7894959213404a693
Daniel Jones <messyjones@gmail.com>
2022-12-10 14:00:27 -0500

    added the ios app into the github repo

---
+++ ios/hackathon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hackathon
+//
+//  Created by Daniel Jones on 12/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
