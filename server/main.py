7a9d8c9e6f127f132b9ea650c2bec2a299599a9d
c3-yushihomma <yushi.homma@c3iot.ai>
2022-12-10 17:28:29 -0800

    Made skeleton app.yml

--- server/main.py
+++ server/main.py
@@ -1,8 +1,24 @@
-from flask import Flask
+from flask import Flask, send_file
 app = Flask(__name__)
 
 @app.route('/karaoke/<string:url>/')
 def get_karaoke(url):
-    return url
+    try:
+        with open("a.txt", "w+") as f:
+            f.write("hello")
+        response = send_file('a.txt')
+        response.headers['lyrics'] = "a"
+    finally:
+        import os
+        os.remove("a.txt")
+    return response
 
-app.run()
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
