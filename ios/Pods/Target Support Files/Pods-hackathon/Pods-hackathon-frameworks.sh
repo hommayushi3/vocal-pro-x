1d69fc4e2a95fcd9afb9541d52769d5150c83928
Daniel Jones <messyjones@gmail.com>
2022-12-11 00:35:21 -0500

    isntalled alamofire

--- ios/Pods/Target Support Files/Pods-hackathon/Pods-hackathon-frameworks.sh
+++ ios/Pods/Target Support Files/Pods-hackathon/Pods-hackathon-frameworks.sh
@@ -176,6 +176,7 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/BRYXBanner/BRYXBanner.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Bolts/Bolts.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Parse/Parse.framework"
@@ -184,6 +185,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/BRYXBanner/BRYXBanner.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Bolts/Bolts.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Parse/Parse.framework"
---
+++ ios/hackathon/karaoke/KaraokeDataStore.swift
@@ -0,0 +1,12 @@
+//
+//  KaraokeDataStore.swift
+//  hackathon
+//
+//  Created by Daniel Jones on 12/11/22.
+//
+
+import Foundation
+
+class KaraokeDataStore {
+    
+}
