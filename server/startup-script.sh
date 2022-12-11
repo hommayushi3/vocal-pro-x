54978e63ad67ee9be0ffd1043977fd836fcb0c96
c3-yushihomma <yushi.homma@c3iot.ai>
2022-12-11 11:06:40 -0800

    Update startup-script.sh

--- server/startup-script.sh
+++ server/startup-script.sh
@@ -1,29 +1,13 @@
-# Install or update needed software
-apt-get update
-apt-get install -yq git supervisor python python-pip python3-distutils
-pip install --upgrade pip virtualenv
-
-# Fetch source code
-export HOME=/root
-git clone https://github.com/GoogleCloudPlatform/getting-started-python.git /opt/app
-
-# Install Cloud Ops Agent
-sudo bash /opt/app/gce/add-google-cloud-ops-agent-repo.sh --also-install
-
-# Account to own server process
-useradd -m -d /home/pythonapp pythonapp
-
-# Python environment setup
-virtualenv -p python3 /opt/app/gce/env
-/bin/bash -c "source /opt/app/gce/env/bin/activate"
-/opt/app/gce/env/bin/pip install -r /opt/app/gce/requirements.txt
-
-# Set ownership to newly created account
-chown -R pythonapp:pythonapp /opt/app
-
-# Put supervisor configuration in proper place
-cp /opt/app/gce/python-app.conf /etc/supervisor/conf.d/python-app.conf
-
-# Start service via supervisorctl
-supervisorctl reread
-supervisorctl update
+#!/bin/bash
+git clone https://github.com/hommayushi3/vocal-pro-x.git
+cd vocal-pro-x
+conda create -n music python=3.8
+conda activate music
+conda deactivate
+conda deactivate
+conda activate music
+pip install -r requirements.txt
+pip install gunicorn torchaudio==0.12.0 torch==1.12.1 triton==2.0.0.dev20221120
+pkill gunicorn
+cd server
+gunicorn --bind 0.0.0.0:5000 main:app --timeout 600
