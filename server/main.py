6518a2dcd9967948e85e190c0ed353545c224df8
Patrick Tan <tan.patrick.river@gmail.com>
2022-12-10 19:13:45 -0800

    api works with returning text

--- server/main.py
+++ server/main.py
@@ -1,16 +1,56 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, jsonify
 app = Flask(__name__)
 
-@app.route('/karaoke/<string:url>/')
-def get_karaoke(url):
+def transcribe(video_id):
+    
+    import os
+    import subprocess
+    import pandas as pd
+    from pytube import YouTube
+    from stable_whisper import load_model
+    from functools import reduce
+    
+    # Download audio stream of YouTube video
+    video = YouTube(f"https://www.youtube.com/watch?v={video_id}")
+
+    video\
+    .streams\
+    .filter(only_audio=True)\
+    .filter(file_extension='mp4')\
+    .order_by('abr')\
+    .last()\
+    .download(output_path = f'{os.getcwd()}/mp4/{video_id}/', filename = f'{video_id}.mp4')
+    
+    # Convert MP4 to WAV
+    subprocess.run([f"audioconvert convert mp4/{video_id}/ wav/ --output-format .wav"], shell = True)
+    
+    # Split Vocals / Instrumentals
+    subprocess.run([f"demucs --two-stems=vocals 'wav/{video_id}.wav'"], shell = True)
+    
+    # Transcribe Lyrics
+    transcription_model = load_model('medium')
+    segments = transcription_model.transcribe(f"{os.getcwd()}/separated/htdemucs/{video_id}/vocals.wav")['segments']
+    
+    lyric_lines = [segment['text'] for segment in segments]
+    word_ts = pd.DataFrame(
+        reduce(lambda x,y: x + y, [segment['word_timestamps'] for segment in segments])
+    ).drop('token', axis = 1)\
+    .to_json(orient = 'records')
+    instrumental_filepath = f"{os.getcwd()}/separated/htdemucs/{video_id}/no_vocals.wav"
+    
+    return {
+        'lyric_lines': lyric_lines,
+        'word_timestamps': word_ts,
+        'instrumental_filepath': instrumental_filepath
+    }
+
+@app.route('/karaoke/<string:video_id>/')
+def get_karaoke(video_id):
     try:
-        with open("a.txt", "w+") as f:
-            f.write("hello")
-        response = send_file('a.txt')
-        response.headers['lyrics'] = "a"
+        output = transcribe(video_id)
+        response = jsonify(output)
     finally:
-        import os
-        os.remove("a.txt")
+        pass
     return response
 
 if __name__ == '__main__':
