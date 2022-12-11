5db43e004c98b79f9c2b3273f24038d0c4d0edf6
Daniel Jones <messyjones@gmail.com>
2022-12-10 21:54:19 -0500

    it highlights words as they play

--- ios/hackathon/karaoke/KaraokeViewController.swift
+++ ios/hackathon/karaoke/KaraokeViewController.swift
@@ -7,10 +7,19 @@
 
 import UIKit
 import SnapKit
+import AVFoundation
+
+struct KWord {
+    let timestamp: Double
+    let word: String
+}
 
 class KaraokeViewController: UIViewController {
     private let karoakeView = UIStackView()
     private var lines: [String] = []
+    private var kWords: [KWord] = []
+    private var timer: Timer?
+    private var player: AVAudioPlayer?
     
     override func loadView() {
         super.loadView()
@@ -29,6 +38,42 @@ class KaraokeViewController: UIViewController {
         navigationItem.leftBarButtonItem = searchBtn
     }
     
+    private func setTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: { timer in
+            self.highlightWord()
+        })
+    }
+    
+    private func highlightWord() {
+        for (index, word) in kWords.enumerated() {
+            if kWords.indices.contains(index + 1) {
+                let nextTime = kWords[index + 1].timestamp
+                let currentTime = player?.currentTime ?? 0.0
+                if currentTime >= word.timestamp && currentTime <= nextTime {
+                    for lineView in karoakeView.arrangedSubviews {
+                        if let lineView = lineView as? UILabel, let text = lineView.text, text.contains(word.word) {
+                            let range = (text as NSString).range(of: word.word)
+
+                            let attributedText = NSMutableAttributedString.init(string: text)
+                            let blue = UIColor.init(red: 48 / 255.0, green: 196 / 255.0, blue: 246 / 255.0, alpha: 1.0)
+                            attributedText.addAttribute(NSAttributedString.Key.foregroundColor, value: blue , range: range)
+                            lineView.attributedText = attributedText
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+    
+    private func findLine(with word: String) -> Int? {
+        let index = lines.firstIndex { line in
+            return line.contains(word)
+        }
+        
+        return index
+    }
+    
     @objc private func searchPressed() {
         removeLine()
     }
@@ -40,7 +85,12 @@ class KaraokeViewController: UIViewController {
                  "I'd buy a big house where we both could live"
         ]
         
+        let word = KWord(timestamp: 1.02, word: "It's")
+        kWords = [word, word, word, word]
+        
         showLines()
+        playSound()
+        setTimer()
     }
     
     private func showLines() {
@@ -118,5 +168,27 @@ class KaraokeViewController: UIViewController {
             blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
             alpha: CGFloat(1.0)
         )
+    }
+    
+    private func playSound() {
+        guard let url = Bundle.main.url(forResource: "john", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
 }
