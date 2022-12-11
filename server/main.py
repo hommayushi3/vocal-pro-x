d1921fc9a9fa3a5040cc5d0603a866c42a80c4fb
c3-yushihomma <yushi.homma@c3iot.ai>
2022-12-10 22:30:20 -0800

    Removed unnecessary files

--- server/main.py
+++ server/main.py
@@ -36,11 +36,12 @@ def transcribe(video_id):
         reduce(lambda x,y: x + y, [segment['word_timestamps'] for segment in segments])
     ).drop('token', axis = 1)\
     .to_json(orient = 'records')
+    instrumental_filepath = f"{os.getcwd()}/separated/htdemucs/{video_id}/no_vocals.wav"
     
     return {
         'lyric_lines': lyric_lines,
         'word_timestamps': word_ts,
-        'video_id': video_id
+        'instrumental_filepath': instrumental_filepath
     }
 
 @app.route('/karaoke/<string:video_id>/')
@@ -48,19 +49,6 @@ def get_karaoke(video_id):
     try:
         output = transcribe(video_id)
         response = jsonify(output)
-    finally:
-        pass
-    return response
-
-@app.route('/audio_files/<string:video_id>/')
-def get_instrumental_file(video_id):
-    try:
-        import os
-        instrumental_filepath = f"{os.getcwd()}/separated/htdemucs/{video_id}/no_vocals.wav"
-        response = send_file(
-            instrumental_filepath,
-            mimetype = 'audio/wav'
-        )
     finally:
         pass
     return response
